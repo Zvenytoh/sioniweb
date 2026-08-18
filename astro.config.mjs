bdb30b18c5719d0ef6d8cfb6d764b1e94c313404
Mehdi ENNAJEH <102108000+Zvenytoh@users.noreply.github.com>
2026-08-18 15:06:50 +0200

    Allow production preview host

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sioniweb.fr',
+  vite: {
+    preview: {
+      allowedHosts: ['sioniweb.medev-tech.fr'],
+    },
+  },
 });
